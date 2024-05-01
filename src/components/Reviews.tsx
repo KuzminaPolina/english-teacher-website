7846d4e376844fd91ca221e54ddd6d87b8758908
Lina_k <Etein@yandex.ru>
2024-05-01 17:11:00 +0300

    avatar

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -17,7 +17,19 @@ const Reviews = () => {
           {reviews.map((reviewItem) => (
             <li className="diagonal p-10">
               <div className="flex items-center gap-5 mb-7">
-                <div className="w-[80px] h-[80px] bg-slate-400 rounded-full"></div>
+                <div className="w-[80px] h-[80px] bg-amber-400 flex items-center justify-center rounded-full">
+                  <svg
+                    width="60px"
+                    height="60px"
+                    viewBox="0 0 16 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m 8 1 c -1.65625 0 -3 1.34375 -3 3 s 1.34375 3 3 3 s 3 -1.34375 3 -3 s -1.34375 -3 -3 -3 z m -1.5 7 c -2.492188 0 -4.5 2.007812 -4.5 4.5 v 0.5 c 0 1.109375 0.890625 2 2 2 h 8 c 1.109375 0 2 -0.890625 2 -2 v -0.5 c 0 -2.492188 -2.007812 -4.5 -4.5 -4.5 z m 0 0"
+                      fill="#754444"
+                    />
+                  </svg>
+                </div>
                 <p className="text-white text-xl font-bold">
                   {reviewItem.name}
                 </p>
